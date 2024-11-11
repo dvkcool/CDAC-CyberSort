d908aa6c5b16945778c5e913c3f3170340fce96a
Shingamba <49290496+Shingamba@users.noreply.github.com>
2024-11-11 12:32:43 +0530

    Update cleaning.py
    trimming and unnecessary space removal

--- Data Cleaning/cleaning.py
+++ Data Cleaning/cleaning.py
@@ -3,9 +3,11 @@ import pandas as pd
 # Load your dataset
 df = pd.read_csv('mapped_data.csv')
 
-# Remove rows where the text in 'your_column' has less than 30 characters
+# First, trim spaces from both ends and then replace continuous spaces with a single space
+df['complaint'] = df['complaint'].str.strip().str.replace(r'\s+', ' ', regex=True).str.strip()
+
+# Remove rows where the text has less than 30 characters
 df = df[df['complaint'].str.len() >= 30]
 
 # Save the cleaned dataset
 df.to_csv('cleaned_dataset.csv', index=False)
-
